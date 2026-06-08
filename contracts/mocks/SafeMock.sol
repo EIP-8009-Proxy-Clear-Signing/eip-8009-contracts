6fe5e9cf857144855752fc756a7f577f617fd720
nikita-kryvoruk-rd <nikita.kryvoruk@redduck.io>
2026-06-08 18:34:55 +0300

    tests: fix naming issues

--- contracts/mocks/SafeMock.sol
+++ contracts/mocks/SafeMock.sol
@@ -4,9 +4,9 @@ pragma solidity ^0.8.27;
 import {Enum} from "@safe-global/safe-smart-account/contracts/libraries/Enum.sol";
 
 contract SafeMock {
-    bytes32 private constant DOMAIN_SEPARATOR_TYPEHASH =
+    bytes32 private constant _DOMAIN_SEPARATOR_TYPEHASH =
         0x47e79534a245952e8b16893a336b85a3d9ea9fa8c573f3d803afb92a79469218;
-    bytes32 private constant SAFE_TX_TYPEHASH =
+    bytes32 private constant _SAFE_TX_TYPEHASH =
         0xbb8310d486368db6bd6f849402fdd73ad53d316b5a4b2644ad6efe0f941286d8;
 
     mapping(address => bool) private _owners;
@@ -57,7 +57,7 @@ contract SafeMock {
                     domainSeparator(),
                     keccak256(
                         abi.encode(
-                            SAFE_TX_TYPEHASH,
+                            _SAFE_TX_TYPEHASH,
                             to,
                             value,
                             keccak256(data),
@@ -78,7 +78,7 @@ contract SafeMock {
         return
             keccak256(
                 abi.encode(
-                    DOMAIN_SEPARATOR_TYPEHASH,
+                    _DOMAIN_SEPARATOR_TYPEHASH,
                     block.chainid,
                     address(this)
                 )
